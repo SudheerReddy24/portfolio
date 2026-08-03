2e0c1ef478f7bac80e902dcc71aa699659abf663
SudheerReddy24 <chintureddy1313@gmail.com>
2026-08-04 01:49:47 +0530

    fix: update API URL for Render deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://sudheerreddy-portfolio-api.onrender.com/'
+    apiUrl: 'https://sudheerreddy-portfolio-api.onrender.com'
 };
